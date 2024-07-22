7a5c41afbfaa1561b0b3b1a1d841006c3621330b
syndicate <ritambala@live.com>
2024-07-23 00:53:53 +0530

    first commit

---
+++ src/main/java/com/scripter/springstarterpack/SpringStarterPackApplication.java
@@ -0,0 +1,13 @@
+package com.scripter.springstarterpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterPackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarterPackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scripter/springstarterpack/SpringStarterPackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scripter.springstarterpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterPackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
